328afa92978c3972efd8f3c1e789cced849112c8
Daniel Lwetabe <daniel.lwetabe@andela.com>
2019-09-23 12:39:56 +0300

    web service start

--- todo/main.go
+++ todo/main.go
@@ -3,9 +3,12 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	const PORT = "8009"
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context){
-		c.String(200, "hello world")
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
 	})
-	r.Run(":8009")
+	r.Run(":" + PORT)
 }
